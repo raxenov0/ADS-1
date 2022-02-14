41e478a47b2a8d8f960b363f2a8caf216f5abdac
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-02-14 19:46:55 +0300

    Update alg.cpp
    Аксенов Роман 21-ИСТ-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,61 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value == 1) {
+		return false;
+	}
+		else 
+			if (value != 2 && value % 2 == 0) {
+				return false;
+				}
+				else
+					if (value != 3 && value % 3 == 0) {
+						return false;
+					}
+					else
+						if (value != 5 && value % 5 == 0) {
+							return false;
+						}
+						else
+							if (value != 7 && value % 7 == 0) {
+								return false;
+							};
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+int i = 0;
+	int num = 2;
+	while (n >= i) {
+		if (checkPrime(num)) {
+			i++;
+		};
+		if (n == i) {
+			return num;
+		};
+		num++;
+	};
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+while (true) {
+		value++;
+		if (checkPrime(value)) {
+			return value;
+		};
+	};
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+int num = 0;
+	int sum = 0;
+	while (num < hbound) {
+		if (checkPrime(num)) {
+			sum += num;
+		};
+		num++;
+	};
+	return sum;
 }
