05f7f2aa892de8e0fbc55409c4a98326948a9f59
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-02-14 21:19:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,20 +5,20 @@
 
 bool checkPrime(uint64_t value) {
 if (value == 1) {
- return false;
- }
- else if (value != 2 && value % 2 == 0) {
-  return false;
-  }
-   else if (value != 3 && value % 3 == 0) {
-    return false;
-    }
-     else if (value != 5 && value % 5 == 0) {
-      return false;
-      }
-       else if (value != 7 && value % 7 == 0) {
-	return false;
-	}
+return false;
+}
+else if (value != 2 && value % 2 == 0) {
+return false;
+}
+else if (value != 3 && value % 3 == 0) {
+return false;
+}
+else if (value != 5 && value % 5 == 0) {
+return false;
+}
+else if (value != 7 && value % 7 == 0) {
+return false;
+}
 return true;
 }
 
@@ -26,22 +26,22 @@ uint64_t nPrime(uint64_t n) {
 int i = 0;
 int num = 2;
 while (n >= i) {
- if (checkPrime(num)) {
-  i++;
-  }
- if (n == i) {
-  return num;
-  }
- num++;
+if (checkPrime(num)) {
+i++;
+}
+if (n == i) {
+return num;
+}
+num++;
 }
 }
 
 uint64_t nextPrime(uint64_t value) {
 while (true) {
- value++;
- if (checkPrime(value)) {
-  return value;
-  }
+value++;
+if (checkPrime(value)) {
+return value;
+}
 }
 }
 
@@ -49,9 +49,9 @@ uint64_t sumPrime(uint64_t hbound) {
 int num = 0;
 int sum = 0;
 while (num < hbound) {
- if (checkPrime(num)) {
-  sum += num;
-  }
+if (checkPrime(num)) {
+sum += num;
+}
 num++;
 }
 return sum;
