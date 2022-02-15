95975b4749e9455f0389f705347457330dc2eb85
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-02-15 19:05:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,23 +5,19 @@
 
 
 bool checkPrime(uint64_t value) {
-	if (value == 1) {
-	  return false;
-	}
-  else
-    if (value != 2 && value % 2 == 0) {
+  if (value == 1) {
+    return false;
+  }
+  else if (value != 2 && value % 2 == 0) {
       return false;
     }
-    else
-      if (value != 3 && value % 3 == 0) {
+    else if (value != 3 && value % 3 == 0) {
         return false;
       }
-      else
-        if (value != 5 && value % 5 == 0) {
+      else if (value != 5 && value % 5 == 0) {
           return false;
         }
-        else
-          if (value != 7 && value % 7 == 0) {
+        else if (value != 7 && value % 7 == 0) {
             return false;
           }
   int sqrtValue = sqrt(value);
